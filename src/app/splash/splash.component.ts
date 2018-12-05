3d489124ff954c00b08bb2a37b4cb0482c4883e9
GKephart <gkephart@cnm.edu>
2018-12-05 15:08:10 -0700

    added composer.json

--- src/app/splash/splash.component.ts
+++ src/app/splash/splash.component.ts
@@ -1,21 +1,8 @@
-import {Component, OnInit} from "@angular/core";
-import {User} from "../shared/interfaces/user";
-import {UserService} from "../shared/services/user.service";
+import {Component} from "@angular/core";
+
 
 @Component({
 	template: require("./splash.component.html")
 })
 
-export class SplashComponent implements OnInit{
-
-	users : User[];
-	constructor( protected userService: UserService) {}
-
-	ngOnInit() {
-		this.getUsers()
-	}
-
-	getUsers() {
-		this.userService.getAllUsers().subscribe(users => this.users = users);
-	}
-}
+export class SplashComponent {}
